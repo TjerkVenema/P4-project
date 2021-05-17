82eb46f9c70b811ee614c1a06bb3dd561ce91d1d
@IanBervoets <ian.bervoets@gmail.com>
2021-05-17 23:03:52 +0200

    gedeeltelijk afgeronde registratie pagina

---
+++ DWF/Repository/RegistratieRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Dapper;
+
+namespace DWF.Repository
+{
+    public class RegistratieRepository
+    {
+        public static Repository repository = new Repository();
+
+        public int CreateAccount(string email, string wachtwoord, string voornaam, string achternaam)
+        {
+            using var connectie = repository.Connect();
+            var numRowEffected = connectie.Execute(
+                "INSERT INTO gebruikers (email, wachtwoord, voornaam, achternaam) VALUES (@Email, @Wachtwoord, @Voornaam, @Achternaam)",
+                param: new {Email = email, Wachtwoord = wachtwoord, Voornaam = voornaam, Achternaam = achternaam});
+            var gebruikerId = connectie.QuerySingle("SELECT LAST_INSERT_ID();");
+            
+            int numeric_id = 0;
+            foreach (var pair in gebruikerId)
+            {
+                numeric_id = (int)pair.Value;
+            }
+            return numeric_id;
+        }
+    }
+}
