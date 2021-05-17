d6d625ee7970fcbc467108f93d6f1da9307a0168
@IanBervoets <ian.bervoets@gmail.com>
2021-05-17 15:06:03 +0200

    progress

---
+++ DWF/Pages/Registratie.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DWF.Pages
+{
+    public class Registratie : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
