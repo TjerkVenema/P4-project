4b9e81c299315d6d282a42e5d83aaf38ced3bf2e
@IanBervoets <ian.bervoets@gmail.com>
2021-05-22 23:09:36 +0200

    kleine database update

--- DWF/Database/portaaldwf_studenten.sql
+++ DWF/Database/portaaldwf_studenten.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
 --
--- Host: localhost    Database: portaaldwf
+-- Host: 127.0.0.1    Database: portaaldwf
 -- ------------------------------------------------------
 -- Server version	8.0.25
 
@@ -48,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-12 16:53:12
+-- Dump completed on 2021-05-22 23:08:27
