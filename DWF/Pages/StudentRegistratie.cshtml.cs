96ef3009445b9502e6e34c8d79d82db0b6c7ab87
@IanBervoets <ian.bervoets@gmail.com>
2021-05-23 16:50:39 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/TjerkVenema/P4-project into main""
    This reverts commit ad9bed2b88eea7101ca3fba279234f0df7f72748.

---
+++ DWF/Pages/StudentRegistratie.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using DWF.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DWF.Pages
+{
+    public class StudentRegistratie : PageModel
+    {
+        [BindProperty, Required(ErrorMessage = "Voer alstublieft een geldig e-mailadres in."),
+         EmailAddress(ErrorMessage = "Voer alstublieft een geldig e-mailadres in.")]
+        public string Email { get; set; }
+
+        [BindProperty, Required(ErrorMessage = "Voer alstublieft een geldig wachtwoord in"), RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,16}$", ErrorMessage = "Uw wachtwoord voldoet niet aan de vereisten.")]
+        public string Wachtwoord { get; set; }
+        
+        [BindProperty, Required(ErrorMessage = "Voer alstublieft uw voornaam in.")]
+        public string Voornaam { get; set; }
+        
+        [BindProperty, Required(ErrorMessage = "Voer alstublieft uw achternaam in.")]
+        public string Achternaam { get; set; }
+        
+        [BindProperty, Required]
+        public string Opleiding { get; set; }
+
+        public string Bericht { get; set; }
+
+        public RegistratieRepository registratieRepository = new RegistratieRepository();
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            bool isDubbel = RegistratieRepository.Isdubbel(Email);
+            if (ModelState.IsValid && !isDubbel)
+            {
+                int gebruiker = registratieRepository.CreateAccount(Email, Wachtwoord, Voornaam, Achternaam, Opleiding,
+                    null);
+                string doel = String.Format("/{0}", gebruiker);
+                Response.Redirect(doel, permanent: true);
+            }
+            else
+            {
+                if (isDubbel)
+                {
+                    Bericht = "Dit e-mailadres is al geregistreerd.";
+                }
+                else
+                {
+                    Bericht = "Voer alstublieft de velden juist in.";
+                }
+            }
+        }
+    }
+}
