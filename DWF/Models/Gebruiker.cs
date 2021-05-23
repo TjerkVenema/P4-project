4dfb1d1164a2a82b46adbabf1211ff584b1e139d
TjerkVenema <tjerk.venema@student.nhlstenden.com>
2021-05-23 20:09:57 +0200

    Triage omgeving voor een groot deel werkend

---
+++ DWF/Models/Gebruiker.cs
@@ -0,0 +1,15 @@
+namespace DWF.Models
+{
+    public class Gebruiker
+    {
+        public int gebruiker_id { get; set; }
+        
+        public string naam { get; set; }
+        
+        public string email { get; set; }
+        
+        public string opleiding { get; set; }
+        
+        public string zakelijknummer { get; set; }
+    }
+}
