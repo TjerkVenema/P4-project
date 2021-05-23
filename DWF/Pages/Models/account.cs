83d397a31eedc87d9953245b5b3c0763d90f8c84
@IanBervoets <ian.bervoets@gmail.com>
2021-05-23 17:01:39 +0200

    references in account.cs kloppen nu

--- DWF/Pages/Models/account.cs
+++ DWF/Pages/Models/account.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DWF;
 
 namespace account.Models
 {
-    public class Account : Database
+    public class Account : database
     {
         public string Email { get; set; }
 
