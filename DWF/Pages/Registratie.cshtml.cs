8f0c4cc342ea654d6f727864ea0ad17210cea0fc
@IanBervoets <ian.bervoets@gmail.com>
2021-05-23 16:50:39 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/TjerkVenema/P4-project into main""
    This reverts commit ad9bed2b88eea7101ca3fba279234f0df7f72748.

---
+++ DWF/Pages/Registratie.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DWF.Pages
+{
+    public class Registratie : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+
+        
+    }
+}
