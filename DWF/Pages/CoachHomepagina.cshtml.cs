c9d61e23a8bc17d2a5253c49710bc2b7491fb859
f0lmer <30975197+f0lmer@users.noreply.github.com>
2021-05-31 09:17:18 +0200

    Revert "Revert "Merge branch 'Folmer' into main""
    This reverts commit f2ff9a3ced511871951a3f5d3c3c809a6e46130e.

---
+++ DWF/Pages/CoachHomepagina.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DWF.Pages
+{
+    public class CoachHomepagina : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ DWF/Pages/PubliekeOpdrachten.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DWF.Pages
+{
+    public class PubliekeOpdrachten : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
