61a449e5324ab170e7fbf6d3ba605190c3b45da2
@IanBervoets <ian.bervoets@gmail.com>
2021-06-07 09:46:43 +0200

    database aanpassingen

--- DWF/Database/portaaldwf_aanvragen_student.sql
+++ DWF/Database/portaaldwf_aanvragen_student.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: portaaldwf
 -- ------------------------------------------------------
--- Server version	8.0.23
+-- Server version	8.0.25
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,12 +28,21 @@ CREATE TABLE `aanvragen_student` (
   `opdracht_id` int NOT NULL,
   `beschrijving` varchar(1000) NOT NULL,
   PRIMARY KEY (`aanvraag_id`),
-  KEY `opdracht_id` (`opdracht_id`),
   KEY `aanvragen_student_ibfk_1_idx` (`gebruiker_id`),
+  KEY `aanvragen_student_ibfk_2_idx` (`opdracht_id`),
   CONSTRAINT `aanvragen_student_ibfk_1` FOREIGN KEY (`gebruiker_id`) REFERENCES `gebruikers` (`gebruiker_id`),
   CONSTRAINT `aanvragen_student_ibfk_2` FOREIGN KEY (`opdracht_id`) REFERENCES `opdrachten` (`opdracht_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aanvragen_student`
+--
+
+LOCK TABLES `aanvragen_student` WRITE;
+/*!40000 ALTER TABLE `aanvragen_student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aanvragen_student` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -44,4 +53,4 @@ CREATE TABLE `aanvragen_student` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-03 17:18:06
+-- Dump completed on 2021-06-07  9:45:36
--- DWF/Database/portaaldwf_studenten.sql
+++ DWF/Database/portaaldwf_studenten.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: portaaldwf
 -- ------------------------------------------------------
--- Server version	8.0.23
+-- Server version	8.0.25
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,6 +29,15 @@ CREATE TABLE `studenten` (
   CONSTRAINT `studenten_ibfk_1` FOREIGN KEY (`gebruiker_id`) REFERENCES `gebruikers` (`gebruiker_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studenten`
+--
+
+LOCK TABLES `studenten` WRITE;
+/*!40000 ALTER TABLE `studenten` DISABLE KEYS */;
+/*!40000 ALTER TABLE `studenten` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -39,4 +48,4 @@ CREATE TABLE `studenten` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-03 17:18:06
+-- Dump completed on 2021-06-07  9:45:37
